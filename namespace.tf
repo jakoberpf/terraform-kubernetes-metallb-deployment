dc0105d5721224040ed3a74922b1085f107f46e8
Jakob Boghdady <58260820+jakoberpf@users.noreply.github.com>
2023-03-01 23:04:56 +0100

    formatting

--- namespace.tf
+++ namespace.tf
@@ -3,14 +3,12 @@ resource "kubernetes_namespace" "metallb" {
     annotations = {
       name = var.namespace
     }
-
     labels = {
       managed-by                           = var.compartment
       "pod-security.kubernetes.io/audit"   = "privileged"
       "pod-security.kubernetes.io/enforce" = "privileged"
       "pod-security.kubernetes.io/warn"    = "privileged"
     }
-
     name = var.namespace
   }
 }
