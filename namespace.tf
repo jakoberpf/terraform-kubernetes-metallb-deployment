5d6926345af369ccc9bd100885e342386682084c
Jakob Boghdady <58260820+jakoberpf@users.noreply.github.com>
2023-03-01 22:07:56 +0100

    make namespace privileged

--- namespace.tf
+++ namespace.tf
@@ -5,7 +5,10 @@ resource "kubernetes_namespace" "metallb" {
     }
 
     labels = {
-      managed-by = var.compartment
+      managed-by                           = var.compartment
+      "pod-security.kubernetes.io/audit"   = "privileged"
+      "pod-security.kubernetes.io/enforce" = "privileged"
+      "pod-security.kubernetes.io/warn"    = "privileged"
     }
 
     name = var.namespace
